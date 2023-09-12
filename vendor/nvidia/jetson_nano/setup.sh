bf6d8ec4d5eedf3d752c1335b7f30b922c4798c1
hmelder <service@hugomelder.com>
2023-09-12 21:08:16 +0200

    Same with directory

--- vendor/nvidia/jetson_nano/setup.sh
+++ vendor/nvidia/jetson_nano/setup.sh
@@ -5,4 +5,4 @@ echo "* Packing payload..."
 # Get directory relative to this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-tar -cpf ${DIR}/payload.tar -C payload .
+tar -cpf ${DIR}/payload.tar -C ${DIR}/payload .
